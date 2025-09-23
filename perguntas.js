83ce97b2a89383c5d8ce398227efdd0cc1ad1510
leonnardo7 <leonardo.ferreira.coelho@escola.pr.gov.br>
2025-09-23 09:08:50 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+criaCartao(
+    'Futebol',
+    'Qual o maior time do mundo?',
+    'O maior time do mundo é o Real Madrid.',
+)
+criaCartao(
+    'Futebol',
+    'Qual o maior campeão da copa do mundo?',
+    'O Brasil é o maior campeão.',
+)
+criaCartao(
+    'Futebol',
+    'Qual o time com mais torcida do Brasil?',
+    'Flamengo.',
+)
+criaCartao(
+    'Futebol',
+    'Qual o maior estadio de futebol do Brasil?',
+    'Maracanã.',
+)
+criaCartao(
+    'Futebol',
+    'Qual jogador foi considerado o rei do futebol?',
+    'Pelé.',
+)
+
+
