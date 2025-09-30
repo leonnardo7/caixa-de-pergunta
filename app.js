46f22e059d26235a0de4bd0fdce3b4231ede52c9
leonnardo7 <leonardo.ferreira.coelho@escola.pr.gov.br>
2025-09-30 09:05:24 -0300

    .

--- app.js
+++ app.js
@@ -2,4 +2,16 @@ function criarCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+    cartao.innerHTML = `
+    <div class="cartao_conteudo">
+                    <h3>Futebol</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <P>Qual o maior time do mundo?</P>
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                       <p>O maior time do mundo é o Real Madrid.</p>
+                    </div>
+                </div>
+                `
+    container.appendChild(cartao)
 }
