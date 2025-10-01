0e6429c5e7dd20364c6ee5b2680a619504396dfb
leonnardo7 <leonardo.ferreira.coelho@escola.pr.gov.br>
2025-10-01 11:06:19 -0300

    .

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ function criarCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+
     cartao.innerHTML = `
     <div class="cartao_conteudo">
-                    <h3>Futebol</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao_conteudo_pergunta">
-                        <P>Qual o maior time do mundo?</P>
+                        <P>${pergunta}</P>
                     </div>
                     <div class="cartao_conteudo_resposta">
-                       <p>O maior time do mundo é o Real Madrid.</p>
+                       <p>${resposta}</p>
                     </div>
                 </div>
                 `
     container.appendChild(cartao)
+
+    let respostaEstaVisivel = false
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('ative', respostaEstaVisivel)
+ }
+ cartao.addEventListener('click', viraCartao)
 }
